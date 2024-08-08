4e3ae2a9e3fd34cb00565f59d8e816bcb5e5721a
alvarobittar <a.bittar@alumno.um.edu.ar>
2024-08-08 01:10:16 -0300

    cambios de imagenes

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/fondomain.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Registrarse" />
+        <Button label="Iniciar Sesion" />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "upper",
+    justifyContent: "upper",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 100,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "upper",
   },
 });
